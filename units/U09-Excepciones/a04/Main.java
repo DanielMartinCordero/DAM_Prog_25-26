f5df53436f1d5f10a03b6d3b78de1c1474c43193
DanielMartinCordero <danikai1902@gmail.com>
2026-04-14 09:36:34 +0200

    feat: activities A03 and A04 added

--- units/U09-Excepciones/a04/Main.java
+++ units/U09-Excepciones/a04/Main.java
@@ -1,4 +1,59 @@
 package a04;
 
+import java.io.*;
+
 public class Main {
+    public static final String RUTA = "C:\\Users\\ALUMNO\\Desktop\\NumerosReales.txt";
+    public static BufferedReader bF = null;
+
+    static double[] recolectarNumeros() throws IOException, FileNotFoundException {
+
+        double[] sumaMedia = new double[2];
+        double suma = 0.0;
+
+        bF = new BufferedReader(new FileReader(RUTA));
+        String letra = bF.readLine();
+
+        if(letra!=null){
+            String[] numeros = letra.split(" ");
+
+            for(int i=0; i<numeros.length; i++){
+                try {
+                    suma += Double.parseDouble(numeros[i]);
+                }
+                catch(NumberFormatException e){
+                    System.out.println("Error al obtener un número");
+                }
+            }
+            sumaMedia[0] = suma;
+            sumaMedia[1] = suma/numeros.length;
+        }
+        return sumaMedia;
+    }
+
+    public static void main(String[] args) {
+
+        try{
+            double[] resultados = recolectarNumeros();
+            System.out.println("La suma de todos los números es: "+resultados[0]);
+            System.out.println("La media aritmética es: "+resultados[1]);
+        }
+        catch(FileNotFoundException e){
+            System.out.println("No se encontro el archivo");
+            System.out.println(e);
+        }
+        catch(IOException e){
+            System.out.println("Error de entrada y/o salida");
+            System.out.println(e);
+        }
+        finally{
+            try {
+                bF.close();
+            }
+            catch(IOException e) {
+                System.out.println("Error de entrada y/o salida");
+                System.out.println(e);
+            }
+        }
+    }
 }
