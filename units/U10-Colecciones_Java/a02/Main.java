eea586d80e01516b6b407ed6f0739f0b3b122480
DanielMartinCordero <danikai1902@gmail.com>
2026-04-24 14:22:18 +0200

    feat: add activity a02 to unit 10

---
+++ units/U10-Colecciones_Java/a02/Main.java
@@ -0,0 +1,34 @@
+package a02;
+/*2. Implementa una aplicación que pida por consola números enteros no negativos hasta que se
+introduzca un -1 (el -1 no se guarda).
+• Los números se irán insertando en una colección, pudiéndose repetir.
+• Al terminar, se mostrará la colección por pantalla.
+• A continuación, se mostrarán todos los valores pares.
+• Por último, se eliminarán todos los múltiplos de 3 usando un iterador y se mostrará por pantalla la
+colección resultante.*/
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Collection<Integer> listaNum = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+
+        System.out.println("Si quiere cerrar el programa, introduzca -1");
+
+        do{
+            System.out.println("Añada un número");
+            num = sc.nextInt();
+            if (num>=0){
+                listaNum.add(num);
+            }
+            else {
+                System.out.println("No se puede añadir a la lista, los números negativos no son válidos.");
+            }
+        }while(num !=-1);
+        System.out.println("Fin del bucle");
+        System.out.println("Resultados: ");
+        System.out.println(listaNum);
+    }
+}
