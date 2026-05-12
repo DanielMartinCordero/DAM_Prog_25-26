c5b30a6d163f2b6bebfb7d8db1a79b1e863a1beb
DanielMartinCordero <danikai1902@gmail.com>
2026-05-12 22:45:39 +0200

    feat: finish activity a10

--- units/U10-Colecciones_Java/a10/Socio.java
+++ units/U10-Colecciones_Java/a10/Socio.java
@@ -1,6 +1,6 @@
 package a10;
 
-public class Socio {
+public class Socio implements Comparable<Socio> {
     private String dni;
     private String nombre;
     Socio(String dni, String nombre){
@@ -12,5 +12,16 @@ public class Socio {
         return "Socio{" + "DNI: " + dni + ", nombre: " + nombre + '}';
     }
 
-
+    @Override
+    public int compareTo(Socio o) {
+        return this.dni.compareTo(o.dni);
+    }
+    @Override
+    public int hashCode(){
+        return dni.hashCode();
+    }
+    @Override
+    public boolean equals(Object otro){
+        return dni.equals(((Socio) otro).dni);
+    }
 }
