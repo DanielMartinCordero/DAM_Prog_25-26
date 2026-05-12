f445f0c61ae2eecbefad2446b3e22b46eebaed7f
DanielMartinCordero <danikai1902@gmail.com>
2026-05-12 09:15:50 +0200

    feat: add activity 11 to unit 10

--- units/U10-Colecciones_Java/a11/Main.java
+++ units/U10-Colecciones_Java/a11/Main.java
@@ -14,38 +14,98 @@ public class Main {
         System.out.println("4. Listar existencias");
         System.out.println("5. Salir");
     }
-    public static void insertarProducto(){
-        System.out.println("Introduzca el código del producto");
-        String clave = sc.next();
-        if(repuestos.containsKey(clave)){
-            System.out.println("El código ya se encuentra en la base de datos, ¿Desea sobreescribir su valor actual "+repuestos.get(clave)+"?");
-            String
+    public static void listarExistencias(){
+        for(String codigo : repuestos.keySet()){
+            System.out.println("CÓDIGO: "+codigo+" CANTIDAD: "+repuestos.get(codigo));
         }
-        System.out.println("Introduzca las unidades en stock");
+    }
+    public static void actualizarStock(){
+        System.out.println("Introduzca el código del producto al que desea actualizar el stock");
+        String producto = sc.nextLine();
+
+        System.out.println("Introduzca la nueva cantidad");
+        int cantidad = sc.nextInt();
+        sc.nextLine();
+
+        if(repuestos.containsKey(producto)){
+            repuestos.put(producto, cantidad);
+        }
+        else{
+            System.out.println("No se encuentra un producto con ese código en el almacén");
+        }
+    }
+    public static void eliminarProducto(){
+        System.out.println("Introduzca el código del producto que desea eliminar");
+        String producto = sc.nextLine();
+
+        if(repuestos.containsKey(producto)){
+            repuestos.remove(producto);
+        }
+        else{
+            System.out.println("No se encuentra un producto con ese código en el almacén");
+        }
+    }
+    public static boolean comprobarExistencia(String producto){
+        boolean sobreescribirProducto = true;
+        if(repuestos.containsKey(producto)){
+            System.out.println("El código ya se encuentra en la base de datos, ¿Desea sobreescribir su valor de stock actual "+repuestos.get(producto)+"?");
+            String siProductoExiste =  sc.next();
+            if(siProductoExiste.equalsIgnoreCase("no")){
+                sobreescribirProducto = false;
+            }
+        }
+        return sobreescribirProducto;
+    }
+    public static void insertarProducto(String producto){
+        System.out.println("Introduzca las unidades en stock del producto "+producto);
         int unidades = sc.nextInt();
-        repuestos.put(clave, unidades);
+        sc.nextLine();
+        repuestos.put(producto, unidades);
     }
     public static void main(String[] args) {
+        repuestos.put("BUJ-025", 100);
+        repuestos.put("ALT-001", 15);
+        repuestos.put("FIL-999", 40);
+        repuestos.put("AMP-010", 8);
+        repuestos.put("COR-102", 5);
+
         byte opcion = 0;
 
-        System.out.println("Seleccione una opción del menú");
-        mostrarMenu();
         do {
+            System.out.println("Seleccione una opción del menú");
+            mostrarMenu();
             try {
                 opcion = sc.nextByte();
+                sc.nextLine();
             } catch (InputMismatchException e) {
                 System.out.println("Debe introducir un número entero");
                 sc.nextLine();
             }
-        }while(opcion == 0);
         switch (opcion){
             case 1 ->{
-                insertarProducto();
+                System.out.println("Introduzca el código del producto");
+                String clave = sc.nextLine();
+                boolean insertarProducto = comprobarExistencia(clave);
+                if(insertarProducto)
+                    insertarProducto(clave);
             }
             case 2 ->{
                 eliminarProducto();
             }
+            case 3 ->{
+                actualizarStock();
+            }
+            case 4->{
+                listarExistencias();
+            }
+            case 5->{
+                System.out.println("Fin del proceso");
+            }
+            default->{
+                System.out.println("No ha introducido un número válido");
+            }
         }
+        }while(opcion == 0);
 
     }
 }
